c497c11e718f6648d717623770e41f93fe81db04
Emma208 <86932981+Emma208@users.noreply.github.com>
2022-07-04 15:48:29 +0100

    updated mpl_squares

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,1 +1,19 @@
 #This is project for data visualization
+import matplotlib.pyplot as plt
+
+input_values = [1,2,3,4,5]
+squares = [1,4,9,16,25]
+
+plt.style.use('seaborn-whitegrid')
+fig,ax=plt.subplots()
+ax.plot(input_values,squares,linewidth=3)
+
+#set chart title and label axes
+ax.set_title("Square numbers",fontsize=24)
+ax.set_xlabel("Value",fontsize=14)
+ax.set_ylabel("square of value",fontsize=14)
+
+#set size of thick labels
+ax.tick_params(axis='both',labelsize=14)
+
+plt.show()
