3e1dfd097c836020a813fcadf3e70894fc94e737
Emma208 <86932981+Emma208@users.noreply.github.com>
2022-07-05 04:17:09 +0100

    Update scatter_squares.py

--- scatter_squares.py
+++ scatter_squares.py
@@ -8,7 +8,7 @@ y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 fig,ax = plt.subplots()
 
-ax.scatter(x_values, y_values, s=20)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues,s=20)
 
 #set chart title and label axes
 ax.set_title("Square Numbers",fontsize = 24)
